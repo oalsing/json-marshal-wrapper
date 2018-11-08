6ce36f6ef2b4d86c3465a3e6a3837bb69333da0a
Oscar Alsing <oscar.alsing@gmail.com>
2018-11-08 11:37:18 +0100

    Fixes typo

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 )
 
-// MarshalWrap returns a the JSON Marshalled string wrapped in key.
+// MarshalWrap returns a the JSON Marshaled string wrapped in key.
 func MarshalWrap(s interface{}, key string) (string, error) {
 	if len(key) < 1 {
 		return "", errors.New("Key must be at least 1 character")
